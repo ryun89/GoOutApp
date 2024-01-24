d33a325f44408a95f74ecf50d7045b386b249433
ryun89 <115915990+ryun89@users.noreply.github.com>
2024-01-24 19:08:44 +0900

    Initial Commit

---
+++ GoOutApp/GoOutAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  GoOutAppApp.swift
+//  GoOutApp
+//
+//  Created by 八久響 on 2024/01/24.
+//
+
+import SwiftUI
+
+@main
+struct GoOutAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
